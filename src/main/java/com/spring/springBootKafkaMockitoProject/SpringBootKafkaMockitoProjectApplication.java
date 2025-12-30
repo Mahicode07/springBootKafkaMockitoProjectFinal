33a0ed3b8577299296a37e16919cdf6527cff910
mgawas <mahendra.gawas@morningstar.com>
2025-12-31 01:41:53 +0530

    sync with notes

--- src/main/java/com/spring/springBootKafkaMockitoProject/SpringBootKafkaMockitoProjectApplication.java
+++ src/main/java/com/spring/springBootKafkaMockitoProject/SpringBootKafkaMockitoProjectApplication.java
@@ -9,5 +9,4 @@ public class SpringBootKafkaMockitoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaMockitoProjectApplication.class, args);
 	}
-
 }
